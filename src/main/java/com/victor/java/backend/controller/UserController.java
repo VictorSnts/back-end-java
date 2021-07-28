b2415d435e29854d2551d7d2f5e5d96d22ad8873
Victor Santos <vfreitass16@icloud.com>
2021-07-27 22:32:25 -0300

    Criando a clase UserDTO e iniciando a implementacao da classe controller

--- src/main/java/com/victor/java/backend/controller/UserController.java
+++ src/main/java/com/victor/java/backend/controller/UserController.java
@@ -1,13 +1,59 @@
 package com.victor.java.backend.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.victor.java.backend.dto.UserDTO;
 
 @RestController // permite a criação de métodos que serão chamados via Web utilizando o protocolo HTTP.
 public class UserController {
 	
-	@GetMapping("/msg")
+	@GetMapping("/msg") // faz com que o metodo a seguir seja executado no mapeamento /msg
 	public String getMessage() {
 		return "Rest Testado";
+	}
+	
+	public static List<UserDTO> users = new ArrayList<UserDTO>();
+	
+	@PostConstruct // indica que o metodo a seguir vai ser exeutado assim que o container for iniciado
+	public void initializesList() { // Inicializa a lista de users ja com alguns usuarios
+		UserDTO userDTO = new UserDTO();
+	    userDTO.setName("Eduardo");
+	    userDTO.setCpf("123");
+	    userDTO.setEndereco("Rua a");
+	    userDTO.setEmail("eduardo@email.com");
+	    userDTO.setPhone("1234-3454");
+	    userDTO.setRegistrationDate(new Date());
+	    
+	    UserDTO userDTO2 = new UserDTO();
+	    userDTO2.setName("Luiz");
+	    userDTO2.setCpf("456");
+	    userDTO2.setEndereco("Rua b");
+	    userDTO2.setEmail("luiz@email.com");
+	    userDTO2.setPhone("1234-3454");
+	    userDTO2.setRegistrationDate(new Date());
+	    
+	    UserDTO userDTO3 = new UserDTO();
+	    userDTO3.setName("Bruna");
+	    userDTO3.setCpf("678");
+	    userDTO3.setEndereco("Rua c");
+	    userDTO3.setEmail("bruna@email.com");
+	    userDTO3.setPhone("1234-3454");
+	    userDTO3.setRegistrationDate(new Date());
+	    
+	    users.add(userDTO);
+	    users.add(userDTO2);
+	    users.add(userDTO3);
+	}
+	
+	@GetMapping("/users") // faz com que o metodo a seguir seja executado no mapeamento /users
+	public List<UserDTO> getUsers() { 
+		return users;
 	}
 }
---
+++ src/main/java/com/victor/java/backend/dto/UserDTO.java
@@ -0,0 +1,62 @@
+package com.victor.java.backend.dto;
+
+import java.util.Date;
+
+public class UserDTO {
+
+	private String name;
+	private String cpf;
+	private String endereco;
+	private String email;
+	private String phone;
+	private Date registrationDate;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Date getRegistrationDate() {
+		return registrationDate;
+	}
+
+	public void setRegistrationDate(Date registrationDate) {
+		this.registrationDate = registrationDate;
+	}
+
+}
